8d389f167cd36dc2a99477b0fc2a1e6b8c7fbf53
Adamos Kyriakou <somada141@gmail.com>
2018-05-24 11:24:57 +1000

    Added a sample dataset under the new `data.py` module.

---
+++ demo/data.py
@@ -0,0 +1,116 @@
+# coding=utf-8
+
+from __future__ import unicode_literals
+
+authors = {
+    "data": [
+        {
+            "author_id": 1,
+            "name_first": "Robert",
+            "name_last": "Jordan",
+        },
+        {
+            "author_id": 2,
+            "name_first": "Brandon",
+            "name_last": "Sanderson",
+        }
+    ]
+}
+
+books = {
+    "data": [
+        {
+            "book_id": 1,
+            "title": "The Eye of the World",
+            "year": 1990
+        },
+        {
+            "book_id": 2,
+            "title": "The Great Hunt",
+            "year": 1990
+        },
+        {
+            "book_id": 3,
+            "title": "The Dragon Reborn",
+            "year": 1991
+        },
+        {
+            "book_id": 4,
+            "title": "The Shadow Rising",
+            "year": 1992
+        },
+        {
+            "book_id": 5,
+            "title": "The Fires of Heaven",
+            "year": 1993
+        },
+        {
+            "book_id": 6,
+            "title": "Lord of Chaos",
+            "year": 1994
+        },
+        {
+            "book_id": 7,
+            "title": "A Crown of Swords",
+            "year": 1996
+        },
+        {
+            "book_id": 8,
+            "title": "The Path of Daggers",
+            "year": 1998
+        },
+        {
+            "book_id": 9,
+            "title": "Winter's Heart",
+            "year": 2000
+        },
+        {
+            "book_id": 10,
+            "title": "Crossroads of Twilight",
+            "year": 2003
+        },
+        {
+            "book_id": 11,
+            "title": "Knife of Dreams",
+            "year": 2005
+        },
+        {
+            "book_id": 12,
+            "title": "The Gathering Storm",
+            "year": 2009
+        },
+        {
+            "book_id": 13,
+            "title": "Towers of Midnight",
+            "year": 2010
+        },
+        {
+            "book_id": 14,
+            "title": "A Memory of Light",
+            "year": 2013
+        },
+    ]
+}
+
+
+author_books = {
+    "data": [
+        {"author_book_id": 1, "author_id": 1, "book_id": 1},
+        {"author_book_id": 2, "author_id": 1, "book_id": 2},
+        {"author_book_id": 3, "author_id": 1, "book_id": 3},
+        {"author_book_id": 4, "author_id": 1, "book_id": 4},
+        {"author_book_id": 5, "author_id": 1, "book_id": 5},
+        {"author_book_id": 6, "author_id": 1, "book_id": 6},
+        {"author_book_id": 7, "author_id": 1, "book_id": 7},
+        {"author_book_id": 8, "author_id": 1, "book_id": 8},
+        {"author_book_id": 9, "author_id": 1, "book_id": 9},
+        {"author_book_id": 10, "author_id": 1, "book_id": 10},
+        {"author_book_id": 11, "author_id": 1, "book_id": 11},
+        {"author_book_id": 12, "author_id": 1, "book_id": 12},
+        {"author_book_id": 13, "author_id": 1, "book_id": 13},
+        {"author_book_id": 14, "author_id": 1, "book_id": 14},
+        {"author_book_id": 15, "author_id": 2, "book_id": 12},
+        {"author_book_id": 16, "author_id": 2, "book_id": 13},
+        {"author_book_id": 17, "author_id": 2, "book_id": 14},
+    ]
+}
